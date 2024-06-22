90466d2db30fbdbbc1c1335beaf5b7204b023ada
birahimdiaz <birahimn923@gmail.com>
2024-06-22 16:56:52 +0200

    Ajouter models Articles

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'main/article_list.html', {'articles': articles})
